08006decc31c6c93201788af984f954c10fd2079
JXJ19 <jongxj-wm20@student.tarc.edu.my>
2023-09-14 23:27:56 +0800

    JXJ

--- app/src/main/java/com/example/earthsustain/DashActivity.kt
+++ app/src/main/java/com/example/earthsustain/DashActivity.kt
@@ -1,15 +1,107 @@
 package com.example.earthsustain
 
+import android.app.Fragment
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.earthsustain.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 
-class DashActivity : AppCompatActivity() {
+class DashActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
 
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var fragmentManager : FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_navigation)
+
+        createNavigation(savedInstanceState)
 
     }
+
+    private fun createNavigation(savedInstanceState: Bundle?){
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+
+        if (savedInstanceState == null){
+            fragmentManager = supportFragmentManager
+            openFragment(HomeFragment())
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean{
+        when(item.itemId){
+            R.id.nav_home -> {
+                showToast("Home Clicked")
+                openFragment(HomeFragment()) }
+            R.id.nav_logout -> {
+                showToast("Logout Clicked")
+                openFragment(HomeFragment())
+            }
+            R.id.nav_profile -> {
+                showToast("Profile Clicked")
+                openFragment(ProfileFragment())
+            }
+            R.id.nav_contact -> {
+                showToast("Contact Clicked")
+                openFragment(HomeFragment())
+            }
+            R.id.nav_email -> {
+                showToast("Email Clicked")
+                openFragment(HomeFragment())
+            }
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            getOnBackPressedDispatcher().onBackPressed()
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun openFragment(fragment : androidx.fragment.app.Fragment){
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
--- app/src/main/java/com/example/earthsustain/MainActivity.kt
+++ app/src/main/java/com/example/earthsustain/MainActivity.kt
@@ -1,107 +1,24 @@
 package com.example.earthsustain
 
-import android.app.Fragment
-import android.content.res.Configuration
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import com.example.earthsustain.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
-import com.google.android.material.navigation.NavigationBarView
-import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
-
-    lateinit var fragmentManager : FragmentManager
-    lateinit var binding : ActivityMainBinding
-    lateinit var toggle : ActionBarDrawerToggle
-
-
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-//        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
+        val loginbtn = findViewById<TextView>(R.id.loginbtn)
 
-        createNavigation()
-
-    }
-
-    private fun createNavigation(){
-        setSupportActionBar(binding.toolbar)
-
-//        when(it.itemId){
-//            R.id.nav_home -> showToast("Home")
-//            R.id.nav_logout -> showToast("Logout")
-//            R.id.nav_profile -> showToast("My Profile")
-//            R.id.nav_contact -> showToast("Contact Us")
-//            R.id.nav_email -> showToast("Email Us")
-//
-//        }
-
-        val toggle = ActionBarDrawerToggle(this,binding.drawerLayout, binding.toolbar, R.string.open, R.string.close)
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        binding.navView.setNavigationItemSelectedListener(this)
-
-
-        fragmentManager = supportFragmentManager
-        openFragment(HomeFragment())
-
-
-    }
-
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean{
-        when(item.itemId){
-            R.id.nav_home -> openFragment(HomeFragment())
-            R.id.nav_logout -> openFragment(HomeFragment())
-            R.id.nav_profile -> openFragment(ProfileFragment())
-            R.id.nav_contact -> openFragment(HomeFragment())
-            R.id.nav_email -> openFragment(HomeFragment())
-
+        loginbtn.setOnClickListener{
+            val intent = Intent(this, DashActivity::class.java)
+//            intent.putExtra("username", "exampleUsername") // Pass any data you need
+//            val username = intent.getStringExtra("username")
+            startActivity(intent)
         }
-        binding.drawerLayout.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-
-    }
-
-    override fun onBackPressed() {
-
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.getOnBackPressedDispatcher().onBackPressed()
-        }
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun openFragment(fragment : androidx.fragment.app.Fragment){
-        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
     }
 
 }
